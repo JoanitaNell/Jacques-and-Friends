b43cf7b45405f068249a668627b6d9856c817c0b
Joanita <joanitan@agilebridge.co.za>
2022-02-10 15:21:55 +0200

    Classes set

--- GradCoinFridayDemo/Program.cs
+++ GradCoinFridayDemo/Program.cs
@@ -16,28 +16,35 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    
 })
 .WithName("GetWeatherForecast");
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+
+
+
+
+//---------------------
+//Classes
+//---------------------
+
+public class UserWallet
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public Guid UserID { get; set; }
+    public Guid WalletID { get; set; }
+
+    public User User { get; set; }
+    public Wallet Wallet { get; set; }
 }
+
+public class User69
+{
+    public Guid UserID { get; set; }
+    public String Email { get; set; }
+}
+
