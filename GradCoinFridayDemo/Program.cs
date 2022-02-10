3ecb73bcf64a84e7626a7febb7810f3647bd59db
Jacques Human <jacquesH@agilebridge.co.za>
2022-02-10 15:16:13 +0200

    Added classes

--- GradCoinFridayDemo/Program.cs
+++ GradCoinFridayDemo/Program.cs
@@ -41,3 +41,22 @@ internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+public class Wallet
+{
+    public Guid WalletID { get; set; }
+    public int Balance { get; set; }
+    public String WalletType { get; set; }
+    
+}
+
+public class Ledger
+{
+    public int LedgerID { get; set; }
+    public Guid WalletID { get; set; }
+    public String TransactionAction { get; set; }
+    public int Amount { get; set; }
+    public String ProcessStatus { get; set; }
+    public int ParentID { get; set; }
+    public Wallet Wallet { get; set; }
+}
